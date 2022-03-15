9acd02680610394822776c78139b499a6b8d4bc8
Masanov <masanov3167@gmail.com>
2022-03-16 00:06:36 +0500

    some bug fixed

--- js/arr.js
+++ js/arr.js
@@ -9,11 +9,11 @@ var openArray = ["open","open menu","open menyu","oupen","opeyn","opein"];
 
 var startChatArray = ["start","start chat","sitart chat","start message","sitart messaj","messej","messaje","messeg","messag"];
 
-var helloArray = ["hi","hay","hai","hiy","hello","helo","helou","hi guys"];
+var helloArray = ["hi","hay","hai","hiy","hello","helo","helou","hi friend"];
 
 var conditionArray = ["very good","fine","fayn","fayne","best","fine are you","are you","a yu","very good are you"];
 
-var goodbyeArray = ["goodbye","bye","bye bye","good bye","ok goodbye","see you"];
+var goodbyeArray = ["goodbye","bye","bye bye","good bye","ok goodbye","see you later"];
 
-var closeChatArray = ["close","close chat","clouse","clouse chat"];
+var closeChatArray = ["close","close chat","clouse","clouse menu"];
 
